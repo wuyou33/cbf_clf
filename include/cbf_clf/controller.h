0e2e0dd46d42b9ab1adcfc50f30789139e098480
Johann Lange <johannlange@yahoo.de>
2018-11-11 13:54:56 -0800

    Created controller.cpp

---
+++ include/cbf_clf/controller.h
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <cmath>
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include <sstream>
+
+std::string name;
---
+++ src/controller.cpp
@@ -0,0 +1,13 @@
+#include "../include/cbf_clf/controller.h"
+
+//using namespace std;
+
+int main(int argc, char** argvc)
+{
+	std::cout << "Hello CMake." << std::endl;
+	std::cout << "Enter your Name: ";
+	std::cin >> name;
+	std::cout << "Hello " << name << std::endl;
+	system("PAUSE");
+	return 0;
+}
