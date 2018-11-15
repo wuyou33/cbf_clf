a3313971e829147e28a0069b17c7f90c4ec8ec56
Johann Lange <johannlange@yahoo.de>
2018-11-15 13:51:55 -0800

    Debugging...

--- include/cbf_clf/input_message_handler.h
+++ include/cbf_clf/input_message_handler.h
@@ -88,7 +88,7 @@ std::tuple<double, double, double, double, double, double, double> get_pose_Hand
     if (algorithm == "zed"){
     zedPose = node_get_pose.subscribe("/zed/pose", 1000, zed_pose_Callback);
     ROS_INFO("Got some more numbers: [%f], [%f], [%f], [%f], [%f], [%f], [%f]", pose_tx, pose_ty, pose_tz, pose_q_x, pose_q_y, pose_q_z, pose_q_w);
-    ros::spin();
+    ros::spinOnce();
     }
     // else if (alogrithm == "other"){
     
