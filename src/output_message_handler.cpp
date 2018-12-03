7ce7d3cd2273290247b3bd6c22124faf330b3d36
Johann Lange <johannlange@yahoo.de>
2018-12-03 11:50:48 -0800

    Debugging...

--- src/output_message_handler.cpp
+++ src/output_message_handler.cpp
@@ -8,6 +8,27 @@ int main(int argc, char** argv){
     ros::Rate loop_rate(omh_loop_rate_); //Needs to be declared AFTER the NodeHandle !
 
     // Publish Pose Data to MAVROS/MAVLink
+
+    pub_pose = node_omh.advertise<geometry_msgs::PoseStamped>("/mavros/setpoint_attitude/attitude",100);
+
+    geometry_msgs::PoseStamped cmd_msg_pose;
+
+    cmd_msg_pose.header.stamp = ros::Time::now();
+    cmd_msg_pose.header.seq=pose_msg_count;
+    cmd_msg_pose.header.frame_id = 1;
+    cmd_msg_pose.pose.position.x = x;
+    cmd_msg_pose.pose.position.y = y;
+    cmd_msg_pose.pose.position.z = z;
+    cmd_msg_pose.pose.orientation.x = qx;
+    cmd_msg_pose.pose.orientation.y = qy;
+    cmd_msg_pose.pose.orientation.z = qz;
+    cmd_msg_pose.pose.orientation.w = qw;
+
+    pub_pose.publish(cmd_msg_pose);
+    ROS_INFO("Publishing /mavros/setpoint_attitude/attitude - [%.2f, %.2f, %.2f], [%.2f, %.2f, %.2f, %.2f]", x, y, z, qx, qy, qz, qw);
+
+    ++pose_msg_count;
+
     send_pose_Handler(node_omh, omh_pose_tx, omh_pose_ty, omh_pose_tz, omh_pose_qx, omh_pose_qy, omh_pose_qz, omh_pose_qw);
     send_throttle_Handler(node_omh, omh_throttle);
 
