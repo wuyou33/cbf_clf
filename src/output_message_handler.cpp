704352558f6b970b19dfaa8a90503b1a819f7d11
Johann Lange <johannlange@yahoo.de>
2018-12-03 16:19:57 -0800

    Debugging...

--- src/output_message_handler.cpp
+++ src/output_message_handler.cpp
@@ -28,7 +28,7 @@ int main(int argc, char** argv){
     }
     ROS_INFO("FCU connection established");
 
-    //send a few setpoints before starting
+    // send a few setpoints before starting
     for(int i = 100; ros::ok() && i > 0; --i){
         // Publish Pose Data to MAVROS/MAVLink
         send_pose_Handler(node_omh, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0);
